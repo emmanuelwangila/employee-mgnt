46fdfee3ee2b6efb9e28cfa7777848174edbf81f
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-06 00:09:47 +0300

    setting up the id

--- src/main/java/employeetech/com/employee/manager/model/Employee.java
+++ src/main/java/employeetech/com/employee/manager/model/Employee.java
@@ -30,4 +30,12 @@ public class Employee implements  Serializable {
         this.imageUrl = imageUrl;
     }
 
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
 }

6af77636493b6bebeb7612507d753d37ceac3875
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-05 23:03:32 +0300

    initail commit

---
+++ src/main/java/employeetech/com/employee/manager/model/Employee.java
@@ -0,0 +1,25 @@
+package employeetech.com.employee.manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+
+import  java.io.Serializable;
+
+
+@Entity
+public class Employee implements  Serializable {
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private String email;
+    private String title;
+    private  String phone;
+    private String name;
+    private  String imageUrl;
+    private String employeeCode;
+
+}

ece2eb7c37249cf46fc33ac91c642317af2a4f2b
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-06 14:21:00 +0300

    getting employee BY id

--- src/main/java/employeetech/com/employee/manager/EmployeeResource.java
+++ src/main/java/employeetech/com/employee/manager/EmployeeResource.java
@@ -6,6 +6,7 @@ import employeetech.com.employee.manager.service.EmployeeService;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,12 @@ public class EmployeeResource {
         List<Employee> employees = employeeService.findAllEmployees();
         return  new ResponseEntity<>(employees , HttpStatusCode.valueOf(200));
 
+    }
+
+    @GetMapping("find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id")  Long id){
+       Employee employee = employeeService.findEmployeeById( id);
+        return  new ResponseEntity<>(employee, HttpStatusCode.valueOf(200));
     }
 
 }

7f82c896efd9d3d4c969febdfaac18db14cb1afa
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-06 13:53:05 +0300

    adding employee service contsructor for the controllers

---
+++ src/main/java/employeetech/com/employee/manager/EmployeeResource.java
@@ -0,0 +1,19 @@
+package employeetech.com.employee.manager;
+
+
+import employeetech.com.employee.manager.model.Employee;
+import employeetech.com.employee.manager.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+}

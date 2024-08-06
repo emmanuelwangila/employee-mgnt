21ad7530c7f477405809fd11eba89977d6e564b4
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-06 12:22:16 +0300

    adding addEmployee method to save employee

--- src/main/java/employeetech/com/employee/manager/service/EmployeeService.java
+++ src/main/java/employeetech/com/employee/manager/service/EmployeeService.java
@@ -1,4 +1,25 @@
 package employeetech.com.employee.manager.service;
 
+import employeetech.com.employee.manager.model.Employee;
+import employeetech.com.employee.manager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return  employeeRepo.save(employee);
+    }
+
+
 }

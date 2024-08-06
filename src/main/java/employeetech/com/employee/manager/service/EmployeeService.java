6b2ca4e519f472877bf665f169e44bc5d773face
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-06 13:43:16 +0300

    adding transactional

--- src/main/java/employeetech/com/employee/manager/service/EmployeeService.java
+++ src/main/java/employeetech/com/employee/manager/service/EmployeeService.java
@@ -4,12 +4,14 @@ import employeetech.com.employee.manager.model.Employee;
 import employeetech.com.employee.manager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 

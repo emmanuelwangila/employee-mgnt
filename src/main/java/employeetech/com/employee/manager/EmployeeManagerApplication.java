c7c4d6e944cabd67e0f3fd84b2529de8daf915f2
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-07 23:00:42 +0300

    cors configuraion

--- src/main/java/employeetech/com/employee/manager/EmployeeManagerApplication.java
+++ src/main/java/employeetech/com/employee/manager/EmployeeManagerApplication.java
@@ -2,12 +2,21 @@ package employeetech.com.employee.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
 
 @SpringBootApplication
 public class EmployeeManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagerApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter(){
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+
 	}
 
 }

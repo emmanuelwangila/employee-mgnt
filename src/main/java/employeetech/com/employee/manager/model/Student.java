6592fc1922c6d4749fb97c4eb9db52b6a0debb3c
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-12 22:44:55 +0300

    setters

--- src/main/java/employeetech/com/employee/manager/model/Student.java
+++ src/main/java/employeetech/com/employee/manager/model/Student.java
@@ -1,16 +1,45 @@
 package employeetech.com.employee.manager.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 
 @Entity
 public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column( nullable = false , updatable = false)
 
     private Long id;
     private String name;
     private String course;
     private String email;
     private Number age;
+
+    public Student () {};
+
+    public Student( String name , String course , String email , Number age){
+        this.name = name ;
+        this.course = course ;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName(){
+        return  name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
 }

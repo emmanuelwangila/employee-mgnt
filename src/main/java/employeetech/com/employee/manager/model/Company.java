a24e9f123d8cb19733288e4abf73c5894fa19d22
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 13:01:29 +0300

    setters and getters

--- src/main/java/employeetech/com/employee/manager/model/Company.java
+++ src/main/java/employeetech/com/employee/manager/model/Company.java
@@ -28,4 +28,23 @@ public class Company implements Serializable {
         this.debt = debt;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone (){
+        return  phone;
+    }
 }

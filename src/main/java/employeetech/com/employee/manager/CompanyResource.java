ad98baffbb3d68465864c907bfcf73700c3bcdc5
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 15:47:23 +0300

    change endpoint for model company

--- src/main/java/employeetech/com/employee/manager/CompanyResource.java
+++ src/main/java/employeetech/com/employee/manager/CompanyResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/companies")
+@RequestMapping("companies")
 public class CompanyResource {
 
     private final CompanyService companyService;

9d06283d3c4a47cc35926c90c898804f9e57b88c
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 10:05:35 +0300

    adding resource for the crud operations

--- src/main/java/employeetech/com/employee/manager/StudentResource.java
+++ src/main/java/employeetech/com/employee/manager/StudentResource.java
@@ -1,10 +1,16 @@
 package employeetech.com.employee.manager;
 
 
+import employeetech.com.employee.manager.service.EmployeeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentResource {
+
+    private final EmployeeService employeeService;
+
+    public StudentResource(EmployeeService employeeService  ) {this.employeeService = employeeService;}
+
 }

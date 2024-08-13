5ca603f60882dceb119d606f52e75f81875b9094
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 08:50:36 +0300

    set up constructor and repo

--- src/main/java/employeetech/com/employee/manager/service/StudentService.java
+++ src/main/java/employeetech/com/employee/manager/service/StudentService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class StudentService {
+      public  final StudentRepo studentRepo;
 
+      public  StudentService(StudentRepo studentRepo) { this.studentRepo = studentRepo;}
 
 }

1472bb45f2dc370a644d1d2269bb8ed43f2cc94c
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 12:58:37 +0300

    adding entity structure

--- src/main/java/employeetech/com/employee/manager/model/Company.java
+++ src/main/java/employeetech/com/employee/manager/model/Company.java
@@ -1,7 +1,31 @@
 package employeetech.com.employee.manager.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+
+@Entity
 public class Company implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    @Column(nullable = false , updatable = false)
+
+    public String name;
+    public String location;
+    public Number branches;
+    public String phone;
+    public Boolean debt;
+
+    public Company () {}
+
+    public  Company(String name , String location , Number branches , String phone , Boolean debt){
+        this.name = name;
+        this.location = location;
+        this.branches = branches;
+        this.phone = phone ;
+        this.debt = debt;
+    }
 
 }

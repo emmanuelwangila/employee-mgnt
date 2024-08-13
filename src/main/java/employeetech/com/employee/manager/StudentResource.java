cee291d4bf796603519de159724e7509febac4f8
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 09:59:10 +0300

    adding request fr endpoints

---
+++ src/main/java/employeetech/com/employee/manager/StudentResource.java
@@ -0,0 +1,10 @@
+package employeetech.com.employee.manager;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentResource {
+}

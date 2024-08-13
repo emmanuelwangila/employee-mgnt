70a08aa244119be2e58132b98bddd74d10864051
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 10:10:45 +0300

    code clean up

--- src/main/java/employeetech/com/employee/manager/StudentResource.java
+++ src/main/java/employeetech/com/employee/manager/StudentResource.java
@@ -1,16 +1,19 @@
 package employeetech.com.employee.manager;
 
 
+import employeetech.com.employee.manager.model.Student;
 import employeetech.com.employee.manager.service.EmployeeService;
+import employeetech.com.employee.manager.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentResource {
 
-    private final EmployeeService employeeService;
-
-    public StudentResource(EmployeeService employeeService  ) {this.employeeService = employeeService;}
 
 }

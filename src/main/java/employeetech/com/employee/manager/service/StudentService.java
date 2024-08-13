99f713b14513bef19d5630fc0844da56e08cc628
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 08:47:28 +0300

    code clean ups

---
+++ src/main/java/employeetech/com/employee/manager/service/StudentService.java
@@ -0,0 +1,13 @@
+package employeetech.com.employee.manager.service;
+
+
+import employeetech.com.employee.manager.repo.StudentRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class StudentService {
+
+
+}

1bb5482a0087567ed1974141efca14d7abbea28e
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-13 13:12:00 +0300

    setters for id

--- src/main/java/employeetech/com/employee/manager/model/Company.java
+++ src/main/java/employeetech/com/employee/manager/model/Company.java
@@ -29,6 +29,15 @@ public class Company implements Serializable {
         this.debt = debt;
     }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }

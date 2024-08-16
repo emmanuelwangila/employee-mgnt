52be51ebd07eb86420d2d67b08be25122d7117bf
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-16 15:19:46 +0300

    adding aservice for the department entity model

--- src/main/java/employeetech/com/employee/manager/service/DepartmentService.java
+++ src/main/java/employeetech/com/employee/manager/service/DepartmentService.java
@@ -1,11 +1,18 @@
 package employeetech.com.employee.manager.service;
 
 import employeetech.com.employee.manager.repo.DepartmentRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class DepartmentService {
 
-    public DepartmentRepo departmentRepo;
+    private final DepartmentRepo departmentRepo;
 
+//    dependency injection .
+    @Autowired
     public DepartmentService( DepartmentRepo departmentRepo) {
         this.departmentRepo = departmentRepo;
     }

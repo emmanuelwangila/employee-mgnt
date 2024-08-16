170b10eeab76efb9423470875ecb4dd335391c9e
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-16 12:17:53 +0300

    model the department correctly

--- src/main/java/employeetech/com/employee/manager/model/Department.java
+++ src/main/java/employeetech/com/employee/manager/model/Department.java
@@ -1,17 +1,34 @@
 package employeetech.com.employee.manager.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.List;
 
+@Entity
 public class Department {
 
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false , unique = true)
     private String name;
     private List<Employee> employees;
+
+    public Department department () {
+        return null;
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return  name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 
 }

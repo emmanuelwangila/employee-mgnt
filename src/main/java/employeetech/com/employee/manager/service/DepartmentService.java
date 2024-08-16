cf692117b9415c0f46e0940c7a1d5e4685aaf5d1
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-16 15:42:33 +0300

    adding service to fetch all list of departments

--- src/main/java/employeetech/com/employee/manager/service/DepartmentService.java
+++ src/main/java/employeetech/com/employee/manager/service/DepartmentService.java
@@ -6,6 +6,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class DepartmentService {
@@ -20,5 +22,9 @@ public class DepartmentService {
 
     public Department addDepartment (Department department){
         return  departmentRepo.save(department);
+    }
+
+    public List<Department> findAllDepartments (){
+        return  departmentRepo.findAll();
     }
 }

cb047407988b4adab9e457e7855f2d7460273cb2
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-16 15:07:21 +0300

    adding repo for department

---
+++ src/main/java/employeetech/com/employee/manager/repo/DepartmentRepo.java
@@ -0,0 +1,7 @@
+package employeetech.com.employee.manager.repo;
+
+import employeetech.com.employee.manager.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepo  extends JpaRepository<Department , Long> {
+}

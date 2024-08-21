bcc645ec84f14d5988a375cedb6119faf862a79f
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-21 20:07:31 +0300

    adding a function to add payroll for an employee

--- src/main/java/employeetech/com/employee/manager/service/PayrollService.java
+++ src/main/java/employeetech/com/employee/manager/service/PayrollService.java
@@ -1,5 +1,6 @@
 package employeetech.com.employee.manager.service;
 
+import employeetech.com.employee.manager.model.Payroll;
 import employeetech.com.employee.manager.repo.PayrollRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,5 +11,9 @@ public class PayrollService {
 
     public PayrollService (PayrollRepo payrollRepo) {
         this.payrollRepo = payrollRepo;
+    }
+
+    public Payroll addNewPayroll(Payroll payroll){
+        return payrollRepo.save(payroll);
     }
 }

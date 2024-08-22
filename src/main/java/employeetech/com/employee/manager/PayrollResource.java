16044dbd8fc3d96dd5395051c4d031253dcbf735
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-22 10:54:42 +0300

    post mapping for payrolls

--- src/main/java/employeetech/com/employee/manager/PayrollResource.java
+++ src/main/java/employeetech/com/employee/manager/PayrollResource.java
@@ -3,8 +3,12 @@ package employeetech.com.employee.manager;
 
 import employeetech.com.employee.manager.model.Payroll;
 import employeetech.com.employee.manager.service.PayrollService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -16,10 +20,17 @@ public class PayrollResource {
         this.payrollService = payrollService;
     }
 
+    @GetMapping("/all")
     public ResponseEntity<List<Payroll>> getAllPayrolls ( ){
         List<Payroll> allPayrolls = payrollService.getAllPayrolls();
         return new ResponseEntity<>(allPayrolls , HttpStatusCode.valueOf(200));
 
+    }
+
+    @PostMapping("/add ")
+    public ResponseEntity<Payroll>  addAllPayrolls (@RequestBody Payroll payroll){
+        Payroll addedPayroll = payrollService.addNewPayroll(payroll);
+        return  new ResponseEntity<>(addedPayroll , HttpStatus.CREATED);
     }
 
 

6c0bdc00ffc579c5beafc32c0a55dc531a990e17
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-22 08:50:02 +0300

    service for getting all payrolls resource

--- src/main/java/employeetech/com/employee/manager/PayrollResource.java
+++ src/main/java/employeetech/com/employee/manager/PayrollResource.java
@@ -1,6 +1,10 @@
 package employeetech.com.employee.manager;
 
+import employeetech.com.employee.manager.model.Payroll;
 import employeetech.com.employee.manager.service.PayrollService;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,11 @@ public class PayrollResource {
 
     public  PayrollResource(PayrollService payrollService){
         this.payrollService = payrollService;
+    }
+
+    @GetMapping("/all")
+    public Payroll getAllPayrolls(Payroll payroll){
+        payrollService.getAllPayrolls(payroll);
+        return new ResponseEntity<>( HttpStatusCode.valueOf(200) )
     }
 }

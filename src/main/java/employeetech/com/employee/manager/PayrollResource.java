22ba66efb1a445470821201ffc365dac14d3ddd0
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-22 10:17:24 +0300

    adding a delete resource for deleting a Payroll

--- src/main/java/employeetech/com/employee/manager/PayrollResource.java
+++ src/main/java/employeetech/com/employee/manager/PayrollResource.java
@@ -38,4 +38,10 @@ public class PayrollResource {
         return  new ResponseEntity<>(updatedPayroll , HttpStatusCode.valueOf(200));
     }
 
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deletePayrolls(@RequestParam Long id){
+        payrollService.DeletePayrolls(id);
+
+    }
+
 }

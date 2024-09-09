6e5f31c4765675003d25df6cacbf43341e56b2cc
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-09-09 15:34:06 +0300

    adding the service

--- src/main/java/employeetech/com/employee/manager/service/UserInfoService.java
+++ src/main/java/employeetech/com/employee/manager/service/UserInfoService.java
@@ -1,7 +1,23 @@
 package employeetech.com.employee.manager.service;
 
+import employeetech.com.employee.manager.model.User;
+import employeetech.com.employee.manager.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserInfoService {
+
+    @Autowired
+    private UserRepo userRepo ;
+
+    @Autowired
+    private  PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public User registerUser (User user ) {
+        user.setPassword(PasswordEncoder.encode(user.getPassword()));
+        return  userRepo.save(user);
+    }
+
 }

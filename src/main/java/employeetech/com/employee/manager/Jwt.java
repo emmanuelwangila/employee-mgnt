101aff94c02a134b3b45c51678a2e39cc36c7802
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-09-09 21:00:38 +0300

    spring security jwt

--- src/main/java/employeetech/com/employee/manager/Jwt.java
+++ src/main/java/employeetech/com/employee/manager/Jwt.java
@@ -2,15 +2,30 @@ package employeetech.com.employee.manager;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.function.Function;
+
 @Component
 public class Jwt {
      private String SECRET_KEY = "secret";
 
      private String extractUsername ( String token ){
          return  extractionClaim(token , Claims :: getSubject);
-     }
+     }{
+
+    }
 
      public Date extractExpiration (String token) {
          return  extractionClaim (token , Claims ::getExpiration);
+     }
+
+     public <T> T extractionClaim (String token , Function<Claims , T> ClaimsResolver) {
+         final Claims claims = extractAllClaims(token);
+         return ClaimsResolver.apply(claims);
+
+     }
+
+     private Claims extractAllClaims(String token ) {
+         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
      }
 }

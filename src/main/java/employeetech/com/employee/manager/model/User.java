baa0d958445eed8546079cefe06378ff010a244b
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-09-13 14:59:29 +0300

    restorev contsructors

--- src/main/java/employeetech/com/employee/manager/model/User.java
+++ src/main/java/employeetech/com/employee/manager/model/User.java
@@ -6,6 +6,7 @@ import jdk.jfr.DataAmount;
 
 @Entity
 @JsonPOJOBuilder
+
 @Table(name = "users")
 
 public class User {
@@ -25,10 +26,44 @@ public class User {
     @Column(nullable = false , unique = true)
     private String email;
 
+    public User () {}
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

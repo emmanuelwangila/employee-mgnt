714c0350999effc58456c8e69f8187b751a5265c
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-09-13 14:55:44 +0300

    clean ups

--- src/main/java/employeetech/com/employee/manager/model/User.java
+++ src/main/java/employeetech/com/employee/manager/model/User.java
@@ -25,46 +25,10 @@ public class User {
     @Column(nullable = false , unique = true)
     private String email;
 
-    public User () {}
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getPassword () {
-        return  password;
-    }
 
-    public void setPassword ( String password) {
-        this.password = password;
-    }
 
-    public String getEmail () {
-        return  email;
-    }
 
-    public void setEmail (String email) {
-        this.email = email;
-    }
-
-    public void  setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
 }

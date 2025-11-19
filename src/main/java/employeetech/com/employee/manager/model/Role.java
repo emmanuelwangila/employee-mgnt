19d4241c9354164d761124ace08af45566ab293d
Emmanuel Wangila <emmanuelwangila1@gmail.com>
2025-11-19 16:36:42 +0300

    Fix error Column fix

--- src/main/java/employeetech/com/employee/manager/model/Role.java
+++ src/main/java/employeetech/com/employee/manager/model/Role.java
@@ -1,7 +1,7 @@
 package employeetech.com.employee.manager.model;
 
 import jakarta.persistence.Table;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
